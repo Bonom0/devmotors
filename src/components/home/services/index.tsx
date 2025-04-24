261ea25dd68c71b388a4fb11f0f28dfffca3e5a1
Bonom0 <caioazbonomo@gmail.com>
2025-04-24 11:42:26 -0300

    Adicionado parte de serviços e map para exibir todos os serviços existentes

--- src/components/home/services/index.tsx
+++ src/components/home/services/index.tsx
@@ -5,21 +5,44 @@ import { HomeProps } from '@/utils/home.type';
 
 export function Services({ object }: HomeProps){
   return(
-    <section className={styles.containerAbout}>
-      <article className={styles.innerAbout}>
-        <h1 className={styles.title}></h1>
-        <p>{object.metadata.about.description}</p>
-      </article>
+    <>
+      <section className={styles.containerAbout}>
+        <article className={styles.innerAbout}>
+          <h1 className={styles.title}>Sobre</h1>
+          <p>{object.metadata.about.description}</p>
+        </article>
 
-      <div className={styles.bannerAbout}>
-        <Image
-          className={styles.imageAbout}
-          alt='Imagem ilustrativa sobre a empresa'
-          quality={100}
-          fill={true}
-          src={object.metadata.about.banner.url}
-        />
-      </div>
-    </section>
+        <div className={styles.bannerAbout}>
+          <Image
+            className={styles.imageAbout}
+            alt='Imagem ilustrativa sobre a empresa'
+            quality={100}
+            fill={true}
+            src={object.metadata.about.banner.url}
+          />
+        </div>
+      </section>
+    
+      <h2 className={styles.servicesTitle}>Conheça nossos serviços</h2>
+
+      <section className={styles.services}>
+        
+        {object.metadata.services.map((service) => (
+          <article key={service.description} className={styles.service}>
+            <div className={styles.innerService}>
+              <Image
+                className={styles.imageService}
+                alt='Imagem do serviço'
+                quality={100}
+                fill={true}
+                src={service.image.url}
+              />
+            </div>
+            <p>{service.description}</p>
+          </article>
+        ))}
+      
+      </section>
+    </>
   )
 }

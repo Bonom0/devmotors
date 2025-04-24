1a52e0ad8bedfef38901255fc55da341205b33f6
Bonom0 <caioazbonomo@gmail.com>
2025-04-23 23:08:47 -0300

    Importado o componente Hero e Icon Phone. Adicionado componente Hero para visualização, juntamente com as propriedades necessárias(heading, buttonTitle, buttonUrl, bannerUrl e icon)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { Submenu } from "@/components/home/submenu";
 import { getDataHome } from "@/utils/actions/get-data";
 import { HomeProps } from "@/utils/home.type";
+import { Hero } from "@/components/home/hero";
+import { Phone } from "lucide-react";
 
 export default async function Home() {
   const data: HomeProps = await getDataHome();
@@ -9,6 +11,13 @@ export default async function Home() {
   return (
     <main>
       <Submenu/>
+      <Hero
+        heading={data.object.metadata.heading}
+        buttonTitle={data.object.metadata.cta_button.title}
+        buttonUrl={data.object.metadata.cta_button.url}
+        bannerUrl={data.object.metadata.banner.url}
+        icon={<Phone size={24} color="#FFF"/>}
+      />
     </main>
   );
 }

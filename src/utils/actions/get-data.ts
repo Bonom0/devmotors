07cdb0766ec0a07a9d94194d71785f68838b3985
Bonom0 <caioazbonomo@gmail.com>
2025-04-24 14:32:35 -0300

    Adicionado function getSubMenu, para fazer requisição dos submenus criados no Cosmic

--- src/utils/actions/get-data.ts
+++ src/utils/actions/get-data.ts
@@ -1,6 +1,8 @@
 export async function getDataHome(){
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects/68091be59482db3d989cdf9f?pretty=true&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata,type`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects/68091be59482db3d989cdf9f?pretty=true&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata,type`, 
+      { next: { revalidate: 120 } }
+    )
 
     if(!res.ok){
       throw new Error("Failed to fetch data")
@@ -10,5 +12,22 @@ export async function getDataHome(){
 
   }catch(err){
     throw new Error("Failed to fetch data")
+  }
+}
+
+export async function getSubMenu(){
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects?pretty=true&query=%7B%22type%22:%22pages%22%7D&limit=10&skip=0&read_key=${process.env.READ_KEY}&depth=1&props=slug,title`, 
+      { next: { revalidate: 120 } }
+    )
+
+    if(!res.ok){
+      throw new Error("Failed to fetch menu data")
+    }
+
+    return res.json();
+
+  } catch (error) {
+    throw new Error("Failed to fetch menu data")
   }
 }

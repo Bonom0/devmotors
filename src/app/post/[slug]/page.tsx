3358720f17c57893e47e7762128e20229ea476c0
Bonom0 <caioazbonomo@gmail.com>
2025-04-24 15:49:10 -0300

    Importado componentes Hero e Container. Adicionado componentes no return

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,6 +1,10 @@
+import { Hero } from '@/components/home/hero';
 import styles from './styles.module.scss'
 import { getItemBySlug } from '@/utils/actions/get-data'
 import { PostProps } from '@/utils/post.type';
+import { Phone } from 'lucide-react';
+import { Container } from '@/components/container';
+import Image from 'next/image';
 
 export default async function Page({ params: { slug } }: {
   params: { slug: string }
@@ -8,8 +12,48 @@ export default async function Page({ params: { slug } }: {
   const { objects }: PostProps = await getItemBySlug(slug);
   
   return (
-    <div>
-      <h1>Página {slug}</h1>
-    </div>
+    <>
+      <Hero
+        heading={objects[0].title}
+        buttonTitle={objects[0].metadata.button.title}
+        buttonUrl={objects[0].metadata.button.url}
+        bannerUrl={objects[0].metadata.banner.url}
+        icon={<Phone size={24} color="#FFF"/>}
+      />
+
+      <Container>
+        <section className={styles.about}>
+          <article className={styles.innerAbout}>
+            <h1 className={styles.title}>
+              {objects[0].metadata.description.title}
+            </h1>
+            <p>
+              {objects[0].metadata.description.text}
+            </p>
+
+            {objects[0].metadata.description.button_active && (
+              <a 
+                href={objects[0].metadata.description.button_url as string}
+                target='_blank'
+                className={styles.link}
+              >
+                {objects[0].metadata.description.button_title}
+              </a>
+            )}
+          </article>
+
+          <div className={styles.bannerAbout}>
+            <Image
+              className={styles.imageAbout}
+              alt={objects[0].title}
+              quality={100}
+              fill={true}
+              src={objects[0].metadata.banner.url}
+            />
+          </div>
+
+        </section>
+      </Container>
+    </>
   )
 }
